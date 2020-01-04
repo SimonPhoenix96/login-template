245d072ac1d3a31652f0c10e49f8f1fee4663ebd
SimonPhoenix96 <diazjustin1995@yahoo.de>
2020-01-04 11:46:06 +0000

    generated blank components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
